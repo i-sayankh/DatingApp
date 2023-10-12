be57e7b0f3a1fa003c0e8f5f71a28f91e0243b46
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-10-12 13:31:49 +0530

    S5: Parent to Child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { AccountService } from './_services/account.service';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,22 +9,12 @@ import { JsonPipe } from '@angular/common';
 })
 
 export class AppComponent implements OnInit {
-  title = 'Dating App';
-  users: any; 
+  title = 'Dating App'; 
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed') 
-    });
   }
 
   setCurrentUser() {
