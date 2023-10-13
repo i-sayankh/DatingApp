a06e2e6ac9ae4adbf0331ae1a2994dfbbb123270
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-10-13 14:23:44 +0530

    S6: Adding a toastr service for notifications

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   model: any = {}
   
-  constructor(public accountService: AccountService, private router: Router) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
   
   ngOnInit(): void {
   }
@@ -20,7 +21,7 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl('/members'),
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     });
   }
 
