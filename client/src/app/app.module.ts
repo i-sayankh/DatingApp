a42f900f98e43e5dacd84d947df51bf0dc23e73e
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-11-18 11:44:06 +0530

    S9: Adding a Photo Gallery

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { JwtInterceptor } from './_interceptor/jwt.interceptor';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
     TestErrorComponent,
