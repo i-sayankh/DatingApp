be90958ae610b5dd53d73d45997c040ac9432339
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-11-25 12:38:50 +0530

    S12: Validation Feedback

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -21,11 +21,11 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup ({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
     });
-    
+
     this.registerForm.controls['password'].valueChanges.subscribe({
       next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     });
