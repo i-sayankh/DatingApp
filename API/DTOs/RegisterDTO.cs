4bd197f97326fd66f89a69e73974874d8c8f6420
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-11-29 21:41:55 +0530

    S12: Updating the API Register Method

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -4,11 +4,15 @@ namespace API.DTOs
 {
     public class RegisterDTO
     {
-        [Required]
-        public string UserName { get; set; }
+        [Required] public string UserName { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
         
         [Required]
-        [StringLength(8, MinimumLength =4)]
-        public string Password { get; set; }
+        [StringLength(8, MinimumLength =4)] public string Password { get; set; }
     }
 }
