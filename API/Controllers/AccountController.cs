c1f50494bc87c0fa45ad22d5bea38f08643ba2b5
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-12-07 14:15:50 +0530

    S13: Cleaning up the MembersService

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,7 +40,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -60,7 +61,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         private async Task<bool> UserExists(string username)
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { Pagination } from 'src/app/_models/pagination';
+import { UserParams } from 'src/app/_models/userParams';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-member-list',
@@ -12,10 +16,19 @@ export class MemberListComponent implements OnInit{
   // members$: Observable<Member[]> | undefined;
   members: Member[] = [];
   pagination: Pagination | undefined;
-  pageNumber = 2;
-  pageSize = 5;
+  userParams: UserParams | undefined;
+  user: User | undefined;
 
-  constructor(private memberService: MembersService) {}
+  constructor(private memberService: MembersService, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if(user) {
+          this.userParams = new UserParams(user);
+          this.user = user;
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
     // this.members$ = this.memberService.getMembers();
@@ -23,7 +36,8 @@ export class MemberListComponent implements OnInit{
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    if(!this.userParams) return
+    this.memberService.getMembers(this.userParams).subscribe({
       next: response => {
         if(response.result && response.pagination) {
           this.members = response.result;
@@ -37,8 +51,8 @@ export class MemberListComponent implements OnInit{
   }
 
   pageChanged(event: any) {
-    if(this.pageNumber !== event.page) {
-      this.pageNumber = event.page;
+    if(this.userParams && this.userParams?.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page;
       this.loadMembers();
     }    
   }
