b19beac3b77896add28fe0d45f0f8beb1b20870d
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-12-08 12:36:14 +0530

    S13: Making the Last Active action filter more optimal

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -18,7 +18,8 @@ namespace API.Services
         {
             var claims = new List<Claim> 
             {
-                new(JwtRegisteredClaimNames.NameId, user.UserName)
+                new(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName, user.UserName),
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
