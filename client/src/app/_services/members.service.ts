07097abe97bcd0c9f547567f1f7fb134db3456c7
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-12-09 23:26:04 +0530

    S13: Restoring Caching for Member Detailed

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -32,6 +32,31 @@ export class MembersService {
         return response;
       })
     );
+  } 
+
+  getMember(username: string) {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+    if(member) return of(member);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    );
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
@@ -56,28 +81,5 @@ export class MembersService {
     params = params.append('pageSize', pageSize);
     
     return params;
-  }
-
-  getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
-    if(member) return of(member);
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-
-  updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index], ...member}
-      })
-    );
-  }
-
-  setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
-  }
-
-  deletePhoto(photoId: number) {
-    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
