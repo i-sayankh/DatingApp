616976b16d72a9f03369119e2bf445ee18591cd4
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2023-12-14 12:49:44 +0530

    S15: Adding a Message Controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,51 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDTO>> CreateMessage(CreateMessageDTO createMessageDTO)
+        {
+            var username = User.GetUsername();
+
+            if(username == createMessageDTO.RecipientUsername.ToLower()) 
+                return BadRequest("You can't send messages to yourself!!!");
+
+            var sender = await _userRepository.GetUserByUsername(username);
+            var recipient = await _userRepository.GetUserByUsername(createMessageDTO.RecipientUsername);
+
+            if(recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDTO.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if(await _messageRepository.SaveAllAsync()) Ok(_mapper.Map<MessageDTO>(message));
+
+            return BadRequest("Failed to Send Message");
+        }
+    }
+}
